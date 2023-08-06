3b7aac479c08223d2c028a6ccbcb6ce406ff4a61
Полина Полянская <ppolina353@yandex.ru>
2023-08-06 19:14:43 +0300

    add courseJS

---
+++ курс JS/01-base-start/theory.js
@@ -0,0 +1,9 @@
+// ========== Operators
+let a = 10
+let b = 5
+let c = 32
+
+c = c + a
+c = c ** 2
+
+console.log(typeof c) 
---
+++ курс JS/02-array-object-start.html/app.js
@@ -0,0 +1,75 @@
+/*// theory
+
+const array = [1, 2, 3, 5, 20, 42]
+// const arrayStrings = ['k', 'j', 'y']
+// const array = new Array(1, 2, 3, 5, 20, 42)
+
+// console.log(array.length)
+// console.log(array[10])
+console.log(array[array.length - 1])
+array[0] = 'privet'
+array[array.length] = 'becon' 
+console.log(array) */
+
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')  
+
+// console.log(inputElement.value)
+
+const notes = ['записать блок про массивы', 'связать Работыша' ]
+
+function render() {
+    // for (let i = 0; i < notes.length; i++) {
+    //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
+    // }   
+
+    for (let note of notes) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
+    }
+}
+
+render()
+
+createBtn.onclick = function () {
+    // listElement.innerHTML =
+    if (inputElement.value.length === 0) {
+        return 
+    }
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(inputElement.value))
+    inputElement.value = ''
+}
+
+function getNoteTemplate(title) {
+    return `
+    <li
+    class="list-group-item d-flex justify-content-between align-items-center"
+    >
+    <span>${title}</span>
+    <span>
+        <span class="btn btn-small btn-success">&check;</span>
+        <span class="btn btn-small btn-danger">&times;</span>
+    </span>
+    </li>
+    `
+}
+
+/*Object theory*/
+/*const person = {
+    firstName: 'Polina',
+    lastName: 'Polyanskaya',
+    year: 2003,
+    hasPet: false,
+    languages: ['ru', 'en', 'tr'],
+    getFullName: function() {
+        console.log(this.firstName + ' ' + this.lastName)
+    }
+}
+
+console.log(person.year + ' ' + person['languages'])
+const key = 'hasPet'
+console.log(person[key])
+person.hasPet = true 
+console.log(person[key])*/
+
+
